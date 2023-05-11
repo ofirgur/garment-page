2a0558a3a28bc08f9420a30c9e7aecea420f7c31
Taras Kozub <taras@browzwear.com>
2023-05-11 11:44:17 +0300

    use memory router when application used as a microFE

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, createMemoryRouter } from 'react-router-dom';
+import { routes } from './components/Router';
 import App from './components/App';
 
 // Mount function to start up the app
-const mount = el => {
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const router =
+    defaultHistory ||
+    createMemoryRouter(routes, {
+      initialEntries: [initialPath],
+    });
+
+  if (onNavigate) {
+    router.subscribe(({ location }) => onNavigate(location));
+  }
+
   const root = createRoot(el);
-  root.render(<App />);
+  root.render(<App router={router} />);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = router.state.location;
+
+      if (pathname !== nextPathname) {
+        router.navigate(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation,
@@ -14,7 +36,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserRouter(routes) });
   }
 }
 
